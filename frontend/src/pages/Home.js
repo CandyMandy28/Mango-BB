6c88e3d08bf1892320cbf9795371cb88004eaa4e
ajaw2 <ajaw2@illinois.edu>
2020-11-05 18:49:29 -0600

    course/home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -18,12 +18,69 @@ export default class Home extends React.Component {
         <div className={"sidebarCont"}>
           <Sidebar></Sidebar>
         </div>
-        <div className={"mainCont"}>
-          <Header as='h1'>Home</Header>
-          <div className={"container"}>
 
+        <div className={"mainCont"}>
+          <Header as='h1'>Course Page</Header>
+          <div class="ui clearing divider"></div>    
+          <div class="ui three column doubling stackable grid container">
+            <div class="row">
+              <div class="column">
+                <p></p>
+              </div>
+              <div class="column">
+                <p></p>
+              </div>
+              <div class="column">
+                <button class="ui labeled icon right floated button">
+                  <i class="add icon"></i>
+                  Add Course
+                </button>
+              </div>
+            </div>
+            <div class="row">
+              <div class="column">
+                <div className={"container"}>
+                  <div class="ui raised very padded text segment"> 
+                    <h1 class="ui header">CS 411</h1>
+                    <h3 class="ui header">Rank: </h3>
+                    <h3 class="ui header">Attendance: </h3>
+                  </div>
+                </div>
+              </div>
+              <div class="column">
+                <div className={"container"}>
+                  <div class="ui raised very padded text segment"> 
+                    <h1 class="ui header">CS 411</h1>
+                    <h3 class="ui header">Rank: </h3>
+                    <h3 class="ui header">Attendance: </h3>
+                  </div>
+                </div>
+              </div>
+              <div class="column">
+                <div className={"container"}>
+                  <div class="ui raised very padded text segment">
+                    <h1 class="ui header">CS 411</h1>
+                    <h3 class="ui header">Rank: </h3>
+                    <h3 class="ui header">Attendance: </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="column">
+                <div className={"container"}>
+                  <div class="ui raised very padded text segment">
+                    <h1 class="ui header">CS 411</h1>
+                    <h3 class="ui header">Rank: </h3>
+                    <h3 class="ui header">Attendance: </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+          
         </div>
+
       </div>
     );
   }
